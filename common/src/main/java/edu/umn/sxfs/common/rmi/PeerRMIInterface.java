62683c148b345d5f87c602ae76b07475d31deca7
prashantc29 <prash.c.29@gmail.com>
2014-05-01 19:44:13 -0500

    RMI interface #4

---
+++ common/src/main/java/edu/umn/sxfs/common/rmi/PeerRMIInterface.java
@@ -0,0 +1,43 @@
+package edu.umn.sxfs.common.rmi;
+
+import java.rmi.Remote;
+import java.rmi.RemoteException;
+
+/**
+ * RMI specification for Peers. Peers will communicate with each other to get
+ * the files in the System. Specifies following three methods: <br>
+ * <ul>
+ *      <li>download: download the file from the peer</li>
+ *      <li>getLoad: get the load of the current peer.</li>
+ *      <li>getChecksum: return the checksum of the file.</li>
+ * </ul>
+ *  
+ * @author prashant
+ *
+ */
+public interface PeerRMIInterface extends Remote{
+	
+	/**
+	 * Returns the file with the corresponding file name.
+	 * @param filename
+	 * @return
+	 * @throws RemoteException
+	 */
+	// TODO prashant discuss on the return type. Mostly will need a different object to store files.
+	public String download(String filename) throws RemoteException;
+
+	/**
+	 * Returns the number of threads running on the current server.
+	 * @return
+	 * @throws RemoteException
+	 */
+	public int getLoad() throws RemoteException;
+	
+	/**
+	 * Returns the check sum  of the passed filename.
+	 * @param filename
+	 * @return
+	 * @throws RemoteException
+	 */
+	public int getChecksum(String filename) throws RemoteException; 
+}
