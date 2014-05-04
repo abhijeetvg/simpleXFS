43823e753cf8d8e0dca4e301bd0dc9120529111b
prashantc29 <prash.c.29@gmail.com>
2014-05-03 19:14:53 -0500

    #3 trackingServerImpl extends UnicastRemoteObject now

--- server/src/main/java/edu/umn/sxfs/server/TrackingServerImpl.java
+++ server/src/main/java/edu/umn/sxfs/server/TrackingServerImpl.java
@@ -2,6 +2,7 @@ package edu.umn.sxfs.server;
 
 import java.io.Serializable;
 import java.rmi.RemoteException;
+import java.rmi.server.UnicastRemoteObject;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Map;
@@ -9,14 +10,16 @@ import java.util.Set;
 
 import edu.umn.sxfs.common.rmi.TrackingServer;
 import edu.umn.sxfs.common.server.PeerInfo;
+import edu.umn.sxfs.common.util.LogUtil;
 
 /**
  * Tracking Server implementation.
  *
  * Created by Abhijeet on 4/28/2014.
  */
-public class TrackingServerImpl implements TrackingServer, Serializable {
+public class TrackingServerImpl extends UnicastRemoteObject implements TrackingServer, Serializable {
 
+	private static final String CLASS_NAME = TrackingServerImpl.class.getSimpleName();
 	private static final long serialVersionUID = 6294308753271868256L;
 
     /**
@@ -25,7 +28,7 @@ public class TrackingServerImpl implements TrackingServer, Serializable {
     private Map<PeerInfo, Set<String>> metaData = new HashMap<PeerInfo, Set<String>>();
     private static TrackingServerImpl instance = null;
 
-    private TrackingServerImpl() {
+    private TrackingServerImpl() throws RemoteException{
     	if(instance != null) {
     		throw new IllegalStateException("Cannot instantiate this class twice.");
     	}
@@ -38,8 +41,14 @@ public class TrackingServerImpl implements TrackingServer, Serializable {
      * @return singleton instance
      */
     public synchronized static TrackingServerImpl getRawInstance() {
+    	final String method = CLASS_NAME + ".getRawInstance()";
         if (null == instance) {
-            instance = new TrackingServerImpl();
+            try {
+				instance = new TrackingServerImpl();
+			} catch (RemoteException e) {
+				LogUtil.log(method, "Remote Exception while creating TrackingServiceImpl. Exiting.");
+				System.exit(1);
+			}
         }
 
         return instance;
