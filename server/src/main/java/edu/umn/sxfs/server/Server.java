1d1b62e5535a56eb0d6cd54c4e4c42c3dccea9c5
prashantc29 <prash.c.29@gmail.com>
2014-05-03 19:06:21 -0500

    #3 refactoring moving start of server to server

--- server/src/main/java/edu/umn/sxfs/server/Server.java
+++ server/src/main/java/edu/umn/sxfs/server/Server.java
@@ -1,7 +1,54 @@
 package edu.umn.sxfs.server;
 
+import java.net.MalformedURLException;
+import java.rmi.Naming;
+import java.rmi.RemoteException;
+import java.rmi.registry.LocateRegistry;
+
+import edu.umn.sxfs.common.constants.RMIConstants;
+import edu.umn.sxfs.common.util.LogUtil;
+import edu.umn.sxfs.common.validator.ContentValidator;
+
 public class Server {
 
+	private final static String CLASS_NAME = Server.class.getSimpleName();
+    private static String rmiIP;
+    private static int rmiPort;
+	
 	public static void main(String[] args) {
+		final String method = CLASS_NAME + ".main()";
+
+		if (2 != args.length) {
+            LogUtil.log(method, "Invalid cli arguments. Usage server <server ip> " +
+                    "<server rmi port> <Configuration filename>");
+            return;
+        }
+		
+        if (!ContentValidator.isValidIp(args[0])) {
+            LogUtil.log(method, args[0] + " is not a valid IP.");
+            return;
+        }
+        rmiIP = args[0];
+
+        if(!ContentValidator.isValidPort(args[1])) {
+            LogUtil.log(method, args[1] + " is not a valid Port.");
+            return;
+        }
+        rmiPort = Integer.parseInt(args[1]);
+
+        System.setProperty("java.rmi.server.hostname", rmiIP);
+        LogUtil.log(method, "Starting server on " + rmiIP + ":" + rmiPort);
+        LogUtil.log(method, "Binding " + RMIConstants.TRACKING_SERVER_SERVICE);
+
+        try {
+            LocateRegistry.createRegistry(rmiPort);
+            Naming.rebind(RMIConstants.TRACKING_SERVER_SERVICE, TrackingServerImpl.getRawInstance());
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        } catch (RemoteException e) {
+            e.printStackTrace();
+        }
+
+        LogUtil.log(method, "DONE Binding " + RMIConstants.TRACKING_SERVER_SERVICE);
 	}
 }
--- server/src/main/java/edu/umn/sxfs/server/TrackingServerImpl.java
+++ server/src/main/java/edu/umn/sxfs/server/TrackingServerImpl.java
@@ -1,20 +1,14 @@
 package edu.umn.sxfs.server;
 
 import java.io.Serializable;
-import java.net.MalformedURLException;
-import java.rmi.Naming;
 import java.rmi.RemoteException;
-import java.rmi.registry.LocateRegistry;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Map;
 import java.util.Set;
 
-import edu.umn.sxfs.common.constants.RMIConstants;
 import edu.umn.sxfs.common.rmi.TrackingServer;
 import edu.umn.sxfs.common.server.PeerInfo;
-import edu.umn.sxfs.common.util.LogUtil;
-import edu.umn.sxfs.common.validator.ContentValidator;
 
 /**
  * Tracking Server implementation.
@@ -25,16 +19,10 @@ public class TrackingServerImpl implements TrackingServer, Serializable {
 
 	private static final long serialVersionUID = 6294308753271868256L;
 
-	private static final String CLASS_NAME = Server.class.getSimpleName();
-
     /**
      * map: Server -> Set of Files
      */
     private Map<PeerInfo, Set<String>> metaData = new HashMap<PeerInfo, Set<String>>();
-    private String rmiIP;
-    private int rmiPort;
-    private static boolean isInitialized = false;
-
     private static TrackingServerImpl instance = null;
 
     private TrackingServerImpl() {
@@ -75,57 +63,5 @@ public class TrackingServerImpl implements TrackingServer, Serializable {
     public synchronized void updateFiles(PeerInfo peerInfo, Set<String> files) throws RemoteException {
         metaData.remove(peerInfo);
         metaData.put(peerInfo, files);
-    }
-
-    public synchronized void startServer(String[] args) {
-    	if(isInitialized) {
-    		return;
-    	}
-        final String method = CLASS_NAME + ".startServer()";
-
-        if (!ContentValidator.isValidIp(args[0])) {
-            LogUtil.log(method, args[0] + " is not a valid IP.");
-            return;
-        }
-        rmiIP = args[0];
-
-        if(!ContentValidator.isValidPort(args[1])) {
-            LogUtil.log(method, args[1] + " is not a valid Port.");
-            return;
-        }
-        rmiPort = Integer.parseInt(args[1]);
-
-        System.setProperty("java.rmi.server.hostname", rmiIP);
-        LogUtil.log(method, "Starting server on " + rmiIP + ":" + rmiPort);
-        LogUtil.log(method, "Binding " + RMIConstants.TRACKING_SERVER_SERVICE);
-
-        try {
-            LocateRegistry.createRegistry(rmiPort);
-            Naming.rebind(RMIConstants.TRACKING_SERVER_SERVICE, TrackingServerImpl.getRawInstance());
-        } catch (MalformedURLException e) {
-            e.printStackTrace();
-        } catch (RemoteException e) {
-            e.printStackTrace();
-        }
-
-        LogUtil.log(method, "DONE Binding " + RMIConstants.TRACKING_SERVER_SERVICE);
-        isInitialized = true;
-    }
-
-    /**
-     * Usage: ip port
-     * @param args
-     */
-    public static void main(String[] args) {
-
-        final String method = CLASS_NAME + ".main()";
-
-        if (2 != args.length) {
-            LogUtil.log(method, "Invalid cli arguments. Usage server <server ip> " +
-                    "<server rmi port> <Configuration filename>");
-            return;
-        }
-
-        TrackingServerImpl.getRawInstance().startServer(args);
     }
 }
